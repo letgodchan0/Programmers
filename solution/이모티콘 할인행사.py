ef26f95dc76da5b63fbc583e257756d073159f2d
letgodchan0 <letgodchan0@gmail.com>
2023-02-13 15:28:29 +0900

    이모티콘 할인행사

---
+++ solution/이모티콘 할인행사.py
@@ -0,0 +1,33 @@
+from itertools import combinations
+def solution(users, emoticons):
+    n, m = len(users), len(emoticons)
+    
+    discount = [40, 30, 20, 10] * m
+    lst = set()
+    for i in combinations(discount, m):
+        lst.add(i)
+    
+    lst = list(map(lambda x : list(x), lst))
+    discounts = sorted(list(lst), reverse = True)
+
+    answer = []
+    for discount in discounts:
+        total, cnt = 0, 0
+        for user in users:
+            user_total = 0
+            for i in range(len(discount)):
+                if user[0] <= discount[i]:
+                    user_total += int((100 - discount[i]) * 0.01 * emoticons[i])
+
+            # 이모티콘 사야함
+            if user_total < user[1]:
+                total += user_total
+            else:
+                cnt += 1
+                
+        answer.append((cnt, total))
+                
+            
+    answer.sort(key = lambda x : (-x[0], -x[1]))
+            
+    return answer[0]
